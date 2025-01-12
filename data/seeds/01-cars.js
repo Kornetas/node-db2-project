a516258153c22313e591a1f9b73bab07f0c1d2b4
Arkadiusz Rak <arek93r@gmail.com>
2025-01-12 17:00:46 +0100

    finally

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
-// STRETCH
+const cars = [
+  {
+    vin: "11111111111111111",
+    make: "toyota",
+    model: "prius",
+    mileage: 215334,
+    title: "clean",
+    transmission: "manual",
+  },
+  {
+    vin: "22222222222222222",
+    make: "ford",
+    model: "focus",
+    mileage: 215000,
+  },
+  {
+    vin: "33333333333333333",
+    make: "toyota",
+    model: "corolla",
+    mileage: 143334,
+    title: "salvage",
+    transmission: "manual",
+  },
+];
+
+exports.seed = function (knex) {
+  return knex("cars")
+    .truncate()
+    .then(() => {
+      return knex("cars").insert(cars);
+    });
+};
